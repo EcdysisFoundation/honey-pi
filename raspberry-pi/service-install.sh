6b5dfad96462d4536ec6b52af7d63baf6e890182
Austin Adee <aadee92@gmail.com>
2020-09-14 16:40:44 -0500

    update install service

--- raspberry-pi/service-install.sh
+++ raspberry-pi/service-install.sh
@@ -7,5 +7,3 @@ sudo systemctl link ./audio-py.service
 sudo systemctl enable dht11-py.service
 sudo systemctl enable hx711-py.service
 sudo systemctl enable audio-py.service
-
-sudo cp 95-identical-card-names.rules /etc/udev/rules.d/95-identical-card-names.rules 
